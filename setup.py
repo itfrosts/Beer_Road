84def1733609881f58abf2e56597d99ab3a93753
itfrosts <34042314+itfrosts@users.noreply.github.com>
2018-11-11 16:04:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
 
-setup(name='beer_road', 
+setup(name='beer_road',
+    version='1.0',
+    description='Collecting beers along the path in Germany',
+    url='https://github.com/itfrosts/Beer_Road',
+    author='itfrosts',
+    packages=['beer_road'],
+    install_requires=[
+        'argparse',
+        'pandas'
+    ]
+
 )
